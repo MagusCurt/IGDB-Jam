57f9c8e4b3167c7ef06a39fa7d6ddd418db9cfaf
Magus Curt <MagusCurt@proton.me>
2025-06-29 20:31:42 -0400

    fixed the viewpoint strtch settings

--- ui/main_ui/main_menu.gd
+++ ui/main_ui/main_menu.gd
@@ -4,4 +4,4 @@ extends CanvasLayer
 
 func _on_start_pressed() -> void:
 	#change scene to the main game
-	get_tree().change_scene_to_file("res://node_2d.tscn")
+	get_tree().change_scene_to_file("res://levels/#base.tscn")
