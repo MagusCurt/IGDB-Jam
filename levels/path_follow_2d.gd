767cc15d801d4047642259a590bf94f2ce49672a
Magus Curt <MagusCurt@proton.me>
2025-06-30 08:26:59 -0400

    worked on adding the tutorial

--- levels/path_follow_2d.gd
+++ levels/path_follow_2d.gd
@@ -6,7 +6,12 @@ var direction: Vector2 = Vector2.RIGHT
 
 @export var activation_time: float = 0.0
 
+@export var rotation_direction: int = 0 # 0 for no rotation, 1 for clockwise, -1 for counter-clockwise
+@export var rotation_max: float = 360
+
 var end_time = 0
+
+@onready var child = get_children()[0]
 
 func _process(delta: float) -> void:
 	if TimeSingleton.ui_time >= activation_time:
@@ -16,8 +21,10 @@ func active(delta: float) -> void:
 	progress_ratio += delta * speed
 
 	if progress_ratio >= 1.0 and end_time == 0:
-		print_debug("end time set")
 		end_time = TimeSingleton.ui_time
+
+	if child:
+		child.rotation_degrees = rotation_direction * progress_ratio * rotation_max
 
 
 	if TimeSingleton.global_time_scale == -1:
